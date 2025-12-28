a85fe57fb12fb3fa268d943f84a3279bd78ed09c
witness nkge <79025245+Witness-nkge@users.noreply.github.com>
2025-12-28 22:16:17 +0200

    Updated the html, styles and script of reservation page

--- web-pages-scripts/reservations.js
+++ web-pages-scripts/reservations.js
@@ -1,19 +1,38 @@
 document.getElementById('reserve-button').addEventListener('click',submitForm)
 
-function submitForm(e) {
-    e.preventDefault()
+function submitForm() {
 
-    if (!document.getElementById('date').value){
-        alert("please input the date for table reservation")
-    } else if (!document.getElementById('radio-one').checked && !document.getElementById('radio-two').checked){
-        alert("please choose where you would like to seat")
-    } else if (document.getElementById('noDiners').value == 0) {
-        alert("please select the number of diners for the table")
-    } else if (!document.getElementById('occasion').value) {
-        alert("please select the occasion for table reservation")
-    } else if (!document.getElementById('time').value) {
-        alert("please select the time for table reservation")
+    const radioOne = document.getElementById('radio-one')
+    const radioTwo = document.getElementById('radio-two')
+    let radioValue 
+    if (radioOne.checked){
+        radioValue = "Indoor Seating"
+    } else if (radioTwo.checked) {
+        radioValue = "Outdoor Seating"
     } else {
-        alert("You have reserved a table")
+        radioValue = ""
     }
+
+    const date = document.getElementById('date').value
+    const diners = document.getElementById('noDiners').value
+    const occasion = document.getElementById('occasion').value
+    const time = document.getElementById('time').value
+
+    if (!radioOne.checked && !radioTwo.checked ) {}
+    else if (!date) {}
+    else if (diners == "") {}
+    else if (occasion == "") {}
+    else if (!time) {}
+    else alert(`
+        You have successfully reserved a table
+        
+        Type of Seating: ${radioValue}
+        Date: ${date}
+        Number of Diners: ${diners}
+        Occasion: ${occasion}
+        Time: ${time}
+
+        If any of the above information is incorrect 
+        please rebook the table with correct details
+        `)
 }
