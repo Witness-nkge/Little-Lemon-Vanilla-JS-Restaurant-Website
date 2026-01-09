3661e4bdb14a53b51db192958cfa83df12aa30cb
witness nkge <79025245+Witness-nkge@users.noreply.github.com>
2026-01-09 11:32:47 +0200

    Moved menu data from script.js to menu-data.js

---
+++ data/menu-data.js
@@ -0,0 +1,24 @@
+//This week special data
+export const menuData = [
+    {
+        image: "assets/greek-salad.jpg",
+        name: "Greek Salad",
+        price: "$12.99",
+        description: "Refreshing salad, made with tomato, lettuce, feta cheese, and olives. Dressed with salt, hot pepper, and olive oil."
+    },
+    {
+        image: "assets/bruchetta.JPG",
+        name: "Brushetta",
+        price: "$16.99",
+        description: "Toasted bread, topped with tomato, prosciutto and cheese.Seasoned with salt and olive oil."
+    },
+    {
+        image: "assets/lemon-dessert.jpg",
+        name: "Lemon Dessert",
+        price: "$8.50",
+        description: "Fresh baked lemon bread coated in salt and sugar. Powdered in citrus and lemon zest."
+    }
+]
+
+
+//Order data
