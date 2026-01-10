4b7fc8d0e9a07d9912d7fa197a62af4ea34b78fd
witness nkge <79025245+Witness-nkge@users.noreply.github.com>
2026-01-10 23:58:44 +0200

    Import menu data and update testimonials

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import {menuData} from './data/menu-data.js'
+
 //Testimonies
 const testimonialsData = [
     {
@@ -6,7 +8,7 @@ const testimonialsData = [
     },
     {
         name: "Alan Chen",
-        testimony:"My Shiba Inu, Mugi, really loved the cozy vibes and delicious food here."
+        testimony:"My Shiba qInu, Mugi, really loved the cozy vibes and delicious food here."
     },
     {
         name: "Casey Lau",
@@ -18,56 +20,38 @@ let testimonialsInnerHtml = ""
 for (let i=0; i < testimonialsData.length; i++){
     testimonialsInnerHtml += `
     <div class="testimony-card">
-        <h3>${testimonialsData[i].name}</h3>
-        <p>${testimonialsData[i].testimony}</p>
+        <p>⭐⭐⭐⭐⭐</p>
+        <p>"${testimonialsData[i].testimony}"</p>
+        <h3>- ${testimonialsData[i].name}</h3>
     </div>
     `
 }
 testimonials.innerHTML = testimonialsInnerHtml
 
 //Menu
-const menuData = [
-    {
-        image: "assets/greek-salad.jpg",
-        name: "Greek Salad",
-        price: "$12.99",
-        description: "Refreshing salad, made with tomato, lettuce, feta cheese, and olives. Dressed with salt, hot pepper, and olive oil."
-    },
-    {
-        image: "assets/bruchetta.JPG",
-        name: "Brushetta",
-        price: "$16.99",
-        description: "Toasted bread, topped with tomato, prosciutto and cheese.Seasoned with salt and olive oil."
-    },
-    {
-        image: "assets/lemon-dessert.jpg",
-        name: "Lemon Dessert",
-        price: "$8.50",
-        description: "Fresh baked lemon bread coated in salt and sugar. Powdered in citrus and lemon zest."
-    }
-]
-
 const menu = document.getElementById('menu')
 let menuInnerHTML = ""
 
-for (let x = 0; x < menuData.length; x++) {
+menuData.forEach((item, index) => {
     menuInnerHTML += `
     <div class="menu-card">
-        <img src="${menuData[x].image}" alt="${menuData[x].name}" />
+        <img src="${item.image}" alt="${item.name}" />
         <div class="menu-card-text">
-            <div class="col-2-styled">
-                <h3>${menuData[x].name}</h3>
-                <h3 style="color: #FF7700">${menuData[x].price}</h3>
+            <div class="menu-card-text-col-2">
+                <h3>${item.name}</h3>
+                <h3 style="color: #FF7700">${item.price}</h3>
             </div>
-            <p>${menuData[x].description}</p>
-            <a href="">
+            <p>${item.description}</p>
+            <a href="./web-pages-html/order.html?item=${index}">
                 <h3>Order for Delivery</h3>
             </a>
         </div>
     </div>
     `
-}
+})
 menu.innerHTML = menuInnerHTML
+
+
 
 
 
