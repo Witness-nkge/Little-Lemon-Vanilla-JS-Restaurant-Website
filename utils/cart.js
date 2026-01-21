88928361b5bd1e92ada3c2c836d7b8ef073f9c58
witness nkge <79025245+Witness-nkge@users.noreply.github.com>
2026-01-21 02:33:09 +0200

    feat: add cart utility functions

--- utils/cart.js
+++ utils/cart.js
@@ -1,1 +1,21 @@
+import {menuData} from "../data/menu-data.js"
+import { getCart, setCart } from "./cart-state.js";
 
+export function addToCart(itemIndex) {
+    let cart = getCart()
+    cart.push(itemIndex);
+    setCart(cart)
+}
+
+export function countCart() {
+    return getCart().length
+}
+
+export function cartTotal() {
+    let total = 0;
+    const cart = getCart()
+    cart.forEach(index => {
+        total += Number(menuData[index].price);
+    });
+    return total;
+}
