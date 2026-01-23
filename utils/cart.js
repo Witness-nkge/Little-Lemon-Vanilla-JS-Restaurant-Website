a38078c72d5c31671ccfa22d5a3f860220b410a5
witness nkge <79025245+Witness-nkge@users.noreply.github.com>
2026-01-23 23:58:00 +0200

    refactor: update cart to store items with quantities

--- utils/cart.js
+++ utils/cart.js
@@ -1,14 +1,25 @@
 import {menuData} from "../data/menu-data.js"
 import { getCart, setCart } from "./cart-state.js";
 
-export function addToCart(itemIndex) {
-    let cart = getCart()
-    cart.push(itemIndex);
-    setCart(cart)
+export function addToCart(itemIndex, itemQty) {
+    const cart = getCart();
+
+    const existingItem = cart.find(item => item.index === itemIndex);
+
+    if (existingItem) {
+        existingItem.qty = itemQty
+    } else {
+        cart.push({
+            index: itemIndex,
+            qty: itemQty
+        });
+    }
+    
+    setCart(cart);
 }
 
 export function countCart() {
-    return getCart().length
+    return getCart().length;
 }
 
 export function cartTotal() {
@@ -19,3 +30,8 @@ export function cartTotal() {
     });
     return total;
 }
+
+
+
+
+
